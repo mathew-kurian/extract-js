9fedda348fe6ceab755f87881ce6eec0d2ea5414
Mathew <mkurian@salesforce.com>
2020-02-26 15:48:13 -0800

    Added package.json

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 function flatten(arr) {
-  return arr.reduce(function (flat, toFlatten) {
-    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  return arr.reduce(function(flat, toFlatten) {
+    return flat.concat(
+      Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten
+    );
   }, []);
 }
 
-const EXTENSIONS = ['.js', '.ts', '.tsx', '.jsx', '.css', '.less', '.scss'];
+const EXTENSIONS = [".js", ".ts", ".tsx", ".jsx", ".css", ".less", ".scss"];
 
 function extract(file, visited = {}) {
-  const data = fs.readFileSync(file, 'utf8');
-  
+  const data = fs.readFileSync(file, "utf8");
+
   const files = [];
   const dirname = path.resolve(__dirname, path.dirname(file));
   const qualifiedFileName = path.resolve(__dirname, file);
@@ -21,51 +23,51 @@ function extract(file, visited = {}) {
   }
 
   const FULL_IMPORT = /import\s+?[\"'](.*?)[\"']/g;
-  for (const match of (data.match(FULL_IMPORT) || [])) {
+  for (const match of data.match(FULL_IMPORT) || []) {
     const groups = FULL_IMPORT.exec(match);
-    
+
     if (groups && groups.length > 0) {
       files.push(path.join(dirname, groups[1]));
     }
   }
 
   const MODULE_IMPORT = /import.*?from\s+?[\"'](.*?)[\"']/g;
-  for (const str of (data.match(MODULE_IMPORT) || [])) {
+  for (const str of data.match(MODULE_IMPORT) || []) {
     const groups = MODULE_IMPORT.exec(str);
-    
+
     if (groups && groups.length > 0) {
       files.push(path.join(dirname, groups[1]));
     }
   }
-  
+
   const filtered = [];
-  loop: for (const file of files) {    
+  loop: for (const file of files) {
     if (path.extname(file)) {
       filtered.push(file);
       continue;
     }
 
     for (const ext of EXTENSIONS) {
-      try{
+      try {
         fs.statSync(file + ext);
         filtered.push(file + ext);
 
         continue loop;
-      } catch(e) {
+      } catch (e) {
         // console.error(e);
       }
     }
 
-    console.log('Could not find', file);
+    console.log("Could not find", file);
   }
-  
+
   const items = [];
 
   [...new Set(filtered).values()].forEach(item => {
-    items.push(item, extract(item, visited))
+    items.push(item, extract(item, visited));
   });
 
-  return flatten(items);
+  return [...new Set(flatten(items)).values()];
 }
 
-console.log(extract('./test/color.js'));
+console.log(extract("./test/color.js"));
